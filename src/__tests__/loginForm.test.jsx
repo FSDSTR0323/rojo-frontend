8456d4030b1a444f1a78f96493214b58add7f640
Marc Maycas <marc.maycas@gmail.com>
2023-06-13 18:34:33 +0200

    LoginForm test completed

--- src/__tests__/loginForm.test.jsx
+++ src/__tests__/loginForm.test.jsx
@@ -2,9 +2,25 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { LoginForm } from '../components/LoginForm/LoginForm';
 
-test('should render a user name input', async () => {
-  render(<LoginForm />);
-  const labelNickname = await screen.findByText('User name');
-  expect(labelNickname).toBeDefined();
-  expect(labelNickname).toHaveTextContent('User name');
+describe('should render a form to login', () => {
+  test('should render a user name input', async () => {
+    render(<LoginForm />);
+    const nicknameLabel = await screen.findByText('User name');
+    expect(nicknameLabel).toBeDefined();
+    expect(nicknameLabel).toHaveTextContent('User name');
+  });
+
+  test('should render a password input', async () => {
+    render(<LoginForm />);
+    const passwordLabel = await screen.findByText('Password');
+    expect(passwordLabel).toBeDefined();
+    expect(passwordLabel).toHaveTextContent('Password');
+  });
+
+  test('should render a Login Button', async () => {
+    render(<LoginForm />);
+    const passwordLabel = await screen.findByText('Login');
+    expect(passwordLabel).toBeDefined();
+    expect(passwordLabel).toHaveTextContent('Login');
+  });
 });
