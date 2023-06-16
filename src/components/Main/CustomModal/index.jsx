1577016afa90d3e75d81575790a2552af8d4e6dc
Marisa <marisavicente1988@gmail.com>
2023-06-17 00:04:44 +0200

    Fixing token error in create user

--- src/components/Main/CustomModal/index.jsx
+++ src/components/Main/CustomModal/index.jsx
@@ -2,30 +2,27 @@ import { Modal, Box } from '@mui/material';
 
 const CustomModal = ({ children, open, onClose }) => {
   return (
-    <>
-      <Modal
-        open={open}
-        onClose={onClose}
-        slotProps={{
-          backdrop: { style: { backgroundColor: 'rgba(0, 0, 0, 0.8' } },
+    <Modal
+      open={open}
+      onClose={onClose}
+      BackdropProps={{
+        style: { backgroundColor: 'rgba(0, 0, 0, 0.8)' },
+      }}
+    >
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          outline: 'none', // Agregar outline: 'none' para evitar el contorno en el cuadro del Modal
         }}
       >
-        <Box
-          sx={{
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            bgcolor: 'background.paper',
-            boxShadow: 24,
-            p: 4,
-          }}
-        >
-          {children}
-        </Box>
-      </Modal>
-    </>
+        {children}
+      </Box>
+    </Modal>
   );
 };
 
 export default CustomModal;
+
