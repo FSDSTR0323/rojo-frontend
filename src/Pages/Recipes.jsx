f438a97f141a8b8bc0298d259d989fe290c5ae83
marc Obiols <marc.obiols.s@gmail.com>
2023-06-19 18:36:28 +0200

    Recipe List

--- src/Pages/Recipes.jsx
+++ src/Pages/Recipes.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { RecipeList } from '../components/RecipesList/RecipesList';
+import { Typography, Button, Container } from '@mui/material';
+import Header from '../components/Header/Header';
 
 export const Recipes = () => {
+  
   const { slug } = useParams();
 
   return (
     <>
-      <h1>Recipes</h1>
-      <p>Estas visitando la pagina de recetas</p>
-      <p>El slug de la URL es: {slug}</p>
+      <Header />
+      <Container
+        maxWidth="lx"
+      >
+      <div>
+        <Button 
+            sx={{ mt: 1.5, mb: 3 }}
+            variant='contained'
+            >
+              Filter
+          </Button>
+        </div>
+        
+        <div>
+          <Typography sx={{ fontSize: 24 }} >
+              Recipes
+          </Typography>
+          <hr></hr>
+          <RecipeList />
+        </div>
+      </Container>
+      
     </>
   );
 };
