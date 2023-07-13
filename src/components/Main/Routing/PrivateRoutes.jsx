cfd3980ffeec75fb8ad0ff611280de0e4b2a733c
Marc Maycas <marc.maycas@gmail.com>
2023-07-13 10:44:16 +0200

    Bugfix - Click twice to go back browser history

--- src/components/Main/Routing/PrivateRoutes.jsx
+++ src/components/Main/Routing/PrivateRoutes.jsx
@@ -1,18 +1,23 @@
-import { Navigate, useLocation, Outlet } from 'react-router-dom';
+import { useLocation, Outlet, useNavigate } from 'react-router-dom';
 import { useUser } from '../../../hooks/useUser';
 import { LOGIN } from '../../../config/routes';
 import { PERMISSIONS_CONFIG } from '../../../config/routes';
 import { DEFAULT_LOGGED_IN_URL } from '../../../config/routes';
+import { useEffect } from 'react';
 
-const Redirect = ({ path }) => (
-  <>
-    <Navigate to={path} />
-    <Outlet />
-  </>
-);
+const Redirect = ({ path }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate(path);
+  }, []);
+
+  return <Outlet />;
+};
 
 const hasPermission = (permissions, path) => {
-  const permission = PERMISSIONS_CONFIG[path];
+  const basePath = `/${path.split('/')[1]}`;
+  const permission = PERMISSIONS_CONFIG[basePath];
   return permission ? permissions.includes(permission) : false;
 };
 
@@ -21,16 +26,16 @@ export const PrivateRoutes = () => {
   const location = useLocation();
 
   const isAuth = user.isLoggedIn;
-  const permissions = user.info.permissions;
+  const permissions = user.info?.permissions;
   const intentPath = location.pathname;
 
   return isAuth ? (
     hasPermission(permissions, intentPath) ? (
       <Redirect path={intentPath} />
     ) : (
-      <Navigate to={DEFAULT_LOGGED_IN_URL} />
+      <Redirect to={DEFAULT_LOGGED_IN_URL} />
     )
   ) : (
-    <Navigate to={LOGIN} />
+    <Redirect to={LOGIN} />
   );
 };
