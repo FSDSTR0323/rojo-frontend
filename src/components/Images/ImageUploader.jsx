7b2a7b29fb5b7562ca3574438971ad55dcbdb63d
Marisa <marisavicente1988@gmail.com>
2023-07-19 17:35:21 +0200

    changes in user table and user admin to fix bugs

--- src/components/Images/ImageUploader.jsx
+++ src/components/Images/ImageUploader.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { CloudUpload, CheckCircle } from '@mui/icons-material';
-import { Button } from '@mui/material';
+import { Button, LinearProgress, Typography, Box } from '@mui/material';
 import Avatar from './avatar';
 
 const ImageUploader = ({ onImageSelect, imageUrl }) => {
@@ -12,28 +12,55 @@ const ImageUploader = ({ onImageSelect, imageUrl }) => {
   const [error, setError] = useState(null);
   const [buttonText, setButtonText] = useState('Upload Image');
 
+  useEffect(() => {
+    if (isUploading) {
+      setButtonText(`Uploading... ${uploadProgress}%`);
+    } else if (isUploadComplete) {
+      setButtonText('Successful upload!');
+    } else {
+      setButtonText('Upload Image');
+    }
+  }, [isUploading, isUploadComplete, uploadProgress]);
+
   const handleImageSelect = (e) => {
     if (e.target.files && e.target.files.length > 0) {
       const file = e.target.files[0];
       setSelectedImage(file);
       setCroppedImage(null);
       setIsUploadComplete(false);      
-      console.log('Selected Image:', file);
       const imageUrl = URL.createObjectURL(file);
-      console.log('Image URL:', imageUrl);
     }
   };
 
   const handleCropImage = (croppedImage) => {
     onImageSelect(croppedImage || selectedImage);
-    console.log('Cropped Image:', croppedImage);
     setCroppedImage(croppedImage);
+  };
+
+  const simulateUploadProgress = () => {
+    if (uploadProgress < 100) {
+      const increment = Math.floor(Math.random() * 5) + 1;
+      setUploadProgress((prevProgress) => Math.min(prevProgress + increment, 100));
+      setTimeout(simulateUploadProgress, 50);
+    } else {
+      const randomError = Math.random() < 0.3;
+      if (randomError) {
+        setError('Error uploading image');
+        setUploadProgress(0);
+      } else {
+        setIsUploadComplete(true);
+        setButtonText('Successful upload!');
+      }
+      setIsUploading(false);
+    }
   };
 
   const handleUpload = async () => {
     if (selectedImage) {
       setUploadProgress(0);
       setError(null);
+      setIsUploadComplete(false);
+      simulateUploadProgress();
       setIsUploading(true);
       setButtonText('Uploading...');
 
@@ -54,33 +81,24 @@ const ImageUploader = ({ onImageSelect, imageUrl }) => {
         if (response.ok) {
           const data = await response.json();
           const imageUrl = data.secure_url;
-          console.log('Uploaded Image URL:', imageUrl);
+          simulateUploadProgress(true);
           onImageSelect(croppedImage);
-          setIsUploadComplete(true);
-          setButtonText('Successful upload!');
         } else {
           throw new Error('Error uploading image');          
         }
-        console.log('Uploaded Image URL:', croppedImage);
       } catch (error) {
-        console.log('Error uploading image:', error);
         setError('Error uploading image');
         setUploadProgress(0);
         setIsUploadComplete(false);
         setButtonText('Upload Image');
       } finally {
         setIsUploading(false);
+        setIsUploadComplete(true);
+        setButtonText('Successful upload!');
       }
     }
   };
 
-  console.log('Selected Image:', selectedImage);
-  console.log('Cropped Image:', croppedImage);
-  console.log('Upload Progress:', uploadProgress);
-  console.log('Is Uploading:', isUploading);
-  console.log('Is Upload Complete:', isUploadComplete);
-  console.log('Error:', error);
-  console.log('Button Text:', buttonText);
 
   return (
     <div>
@@ -88,7 +106,7 @@ const ImageUploader = ({ onImageSelect, imageUrl }) => {
         <>
           <div>
             {selectedImage && (
-              <Avatar image={selectedImage} onCropImage={handleCropImage} />
+              <Avatar image={selectedImage} onCropImage={handleCropImage} sx={{ borderRadius: '50%' }} />
             )}
             
             <Button
@@ -110,17 +128,28 @@ const ImageUploader = ({ onImageSelect, imageUrl }) => {
         </>
       )}
 
-      {croppedImage && <img src={croppedImage} alt="Cropped Image" />}
+      {croppedImage &&
+      <Box sx={{borderRadius:'50%', width:'200px', height:'200px', overflow:'hidden', mb:2}}>
+        <img src={croppedImage} alt="Cropped Image" />
+      </Box> }
+
 
       {uploadProgress > 0 && uploadProgress < 100 && (
         <>
-          <progress max="100" value={uploadProgress} />
-          <span> {uploadProgress} % </span>
+          <LinearProgress
+            max={100}
+            variant="determinate"
+            value={uploadProgress}
+            sx={{ backgroundColor: '#b7b7b7', '& .MuiLinearProgress-bar': { backgroundColor: '#277c27' }, mt: 2 }}
+          />
+          <Typography variant="body1" align="center" sx={{ mt: 1 }}>
+            {uploadProgress} % 
+          </Typography>
         </>
       )}
       {error && <p>Error: {error}</p>}
 
-      {imageUrl && <img src={imageUrl} alt="Selected Image" />}
+      {imageUrl && <img src={imageUrl} alt="Selected Image"  />}
 
       <Button fullWidth onClick={handleUpload} sx={{ mt: 1.5, mb: 3, backgroundColor:"#277c27fb", "&:hover": {backgroundColor: "#277c27cf"}, }} variant="contained"disabled={!selectedImage || isUploading}>
         {isUploading ? (
